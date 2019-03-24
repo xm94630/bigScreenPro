c18b697d683685ebcd3e186ec2ff574145d1a008
xm94630 <xm94630@126.com>
2019-03-24 23:23:28 +0800

    动态添加组件

---
+++ src/components/bee/beeTitle.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="beeTitle">
+    <div @click="myFun" v-bind:class="{ 'canControl': isActive,'baseBox':true}">
+      我是标题
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'xm',
+  props: {
+    msg: String
+  },
+  data(){
+    return{
+        isActive:false
+    }
+  },
+  methods: {
+    myFun(){
+      this.isActive = !this.isActive;
+      console.log(this.isActive)
+    }
+  },
+  
+}
+</script>
+
+<style scoped>
+.beeTitle{
+  color:#fff;
+}
+.baseBox{
+  border:dashed 1px #000;
+  box-sizing:border-box;
+  display:inline-block;
+  cursor:move;
+}
+.canControl{
+  border:dashed 1px red;
+}
+</style>
