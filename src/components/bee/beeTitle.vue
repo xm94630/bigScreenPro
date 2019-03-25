e0f955e8964cfec3fc7ab243159faf36197401fd
xm94630 <xm94630@126.com>
2019-03-25 10:16:35 +0800

    样式

--- src/components/bee/beeTitle.vue
+++ src/components/bee/beeTitle.vue
@@ -10,6 +10,7 @@
       <div class="miniDot dot7"></div>
       <div class="miniDot dot8"></div>
       <div class="miniDot dot9"></div>
+      <div class="showPosition">100,100</div>
     </div>
   </div>
 </template>
@@ -45,11 +46,13 @@ export default {
   border:dashed 1px #000;
   box-sizing:border-box;
   display:inline-block;
-  cursor:move;
   position:relative;
+  user-select:none;
+  cursor:pointer;
 }
 .canControl{
   border:dashed 1px red;
+  cursor:move;
   .miniDot{
     width:5px;
     height:5px;
@@ -59,39 +62,65 @@ export default {
   .dot1{
     top:0px;
     left:0px;
+    cursor:nwse-resize;
   }
   .dot2{
     top:0px;
     left:50%;
+    cursor:ns-resize;
   }
   .dot3{
     top:0px;
     right:0px;
+    cursor:nesw-resize;
   }
   .dot4{
     top:50%;
     left:0;
+    cursor:ew-resize;
   }
   .dot6{
     top:50%;
     right:0px;
+    cursor:ew-resize;
   }
   .dot7{
     bottom:0px;
     left:0px;
+    cursor:nesw-resize;
   }
   .dot8{
     bottom:0px;
     left:50%;
+    cursor:ns-resize;
   }
   .dot9{
     bottom:0px;
     right:0px;
+    cursor:nwse-resize;
+  }
+
+  .showPosition{
+    display:block;
   }
 }
 .wenzi{
   text-align:center;
   position:relative;
+}
+
+.showPosition{
+  position:absolute;
+  left:-60px;
+  top:-20px;
+  width:60px;
+  height:20px;
+  display:none;
+  border-right:dashed 1px red;
+  border-bottom:dashed 1px red;
+  text-align:right;
+  box-sizing:border-box;
+  padding:2px 5px;
 }
 
 
