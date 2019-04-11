822bc7e9546982a1f77ba1752eb2f8fa76055438
xm94630 <xm94630@126.com>
2019-04-11 11:40:06 +0800

    完成title组件的多个渲染输出。这个组件相对简单，没有数据数据源的问题，接下来，我要完成一个需要数据源的折线图，要是完成了，基本上渲染的大流程就完成了！！

--- src/components/reportShow.vue
+++ src/components/reportShow.vue
@@ -51,31 +51,26 @@ export default {
     }
   },
   updated() {
-    //title组件的数据（单个）
-    let titleData = this.reportConfig.components.title;
-    let len = titleData.length;
-
-    console.log(len)
-
-    //循环输出title
-    for(let i=0;i<len;i++){
-      let title_propsConfig = {
-        myConfig: titleData[i]
-      };
-      import("../components/bee/title.vue").then(cmp => {
-        mountCmp(
-          cmp,
-          title_propsConfig,
-          document.querySelector(".myReportCanvas")
-        );
-      });
+    //对配置的的全部组件进行渲染
+    for (let key in this.reportConfig.components) {
+      //渲染全部的“title”组件进行渲染
+      if ("title" == key) {
+        let titleData = this.reportConfig.components.title;
+        let len = titleData.length;
+        for (let i = 0; i < len; i++) {
+          let title_propsConfig = {
+            myConfig: titleData[i]
+          };
+          import("../components/bee/title.vue").then(cmp => {
+            mountCmp(
+              cmp,
+              title_propsConfig,
+              document.querySelector(".myReportCanvas")
+            );
+          });
+        }
+      }
     }
-
-
-
-
-
-
   }
 };
 </script>
