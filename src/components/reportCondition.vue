f77dc6e22a0237bccbecc53bb9de6dcf01d7783b
xm94630 <xm94630@126.com>
2019-05-21 16:45:50 +0800

    继续 全局的条件查询

--- src/components/reportCondition.vue
+++ src/components/reportCondition.vue
@@ -9,12 +9,37 @@
       <div class="reportConditionBtn" @click="clickFun2">
         <i class="el-icon-close"></i>
       </div>
+      
+      <!-- 复用二维表条件查询 -->
+      <div class="reportConditionConBox">
+        <h1 class="MB20">请选择全局查询条件</h1>
+        <el-row>
+          <component
+          v-for="(item, index) in items"
+          :key="index"
+          :is="item.type"
+          :item="item"
+          :ref="item.keyName"
+          @sonChange = "sonChangeHandle"
+          ></component>
+        </el-row>
+        <div class="goRight">
+          <el-button class="btn" ref="searchBtn" type= "primary" @click="OkFun">确定</el-button>
+        </div>
+      </div>
+
+
     </div>
 
   </div>
 </template>
 
 <script>
+import beeInput from './bee/table/beeInput.vue';
+import beeInputRange from './bee/table/beeInputRange.vue';
+import beeDatePicker from './bee/table/beeDatePicker.vue';
+import beeSelect from './bee/table/beeSelect.vue';
+
 export default {
   name: 'xm',
   props: {
@@ -22,8 +47,40 @@ export default {
   },
   data(){
     return{
-      showCon:false,
+      showCon:true,
+      conditionData:{},
+      items:[{
+        span: 12,
+        label: "姓名",
+        keyName: '这个是用来绑定的',
+        type: 'beeInput',
+        placeholder: '请输入',
+        value: '',
+        rule: {},
+      },{
+        span: 12,
+        label: "喜爱",
+        keyName: '这个是用来绑定的',
+        type: 'beeSelect',
+        placeholder: '请输入',
+        value: '',
+        rule: {},
+      },{
+        span: 12,
+        label: "选择日期",
+        keyName: '这个是用来绑定的',
+        type: 'beeDatePicker',
+        placeholder: '日期选择',
+        value: '',
+        rule: {},
+      },],
     }
+  },
+  components:{
+    beeInput,
+    beeInputRange,
+    beeDatePicker,
+    beeSelect,
   },
   methods:{
     clickFun(){
@@ -31,6 +88,13 @@ export default {
     },
     clickFun2(){
       this.showCon = false;
+    },
+    //接受子组件中值的变化，更新数据
+    sonChangeHandle(v,item){
+      this.conditionData[item.keyName] = v;
+    },
+    OkFun(){
+      alert(123);
     }
   }
 }
@@ -58,8 +122,18 @@ export default {
 .reportConditionCon{
   width: 100%;
   height:100%;
-  background-color:rgba(0,0,0,0.8);
+  background-color:rgba(0, 0, 0, 0.9);
   position: absolute;
   top:0;
 }
+.reportConditionConBox{
+  background: #fff;
+  width:1000px;
+  margin: 0 auto;
+  padding:20px;
+  min-height:200px;
+}
+.reportConditionConBox.MB20{margin-bottom: 20px;}
+.reportConditionConBox .goRight{text-align: right}
+.reportConditionConBox .btn{width: 100px;}
 </style>
