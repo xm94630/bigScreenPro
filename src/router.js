5171e80064c37b21734084755505d92f05b74eac
xm94630 <xm94630@126.com>
2019-05-24 18:05:14 +0800

    代码优化

--- src/router.js
+++ src/router.js
@@ -9,30 +9,16 @@ import myReport from './pages/myReport.vue';
 
 Vue.use(VueRouter)
 
-const routes = [
-    {
-        path:'/',
-        component:pageIndex
-    },{
-        path:'/pagea',
-        component:pageA
-    },{
-        path:'/pageb',
-        component:pageB
-    },{
-        path:'/createBigScreen',
-        component:createBigScreen
-    },{
-        path:'/myReport',
-        component:myReport
-    }
 
-
-    
-]
 
 const router = new VueRouter({
-    routes,
+    routes:[
+        {path:'/',component:pageIndex},
+        {path:'/pagea',component:pageA},
+        {path:'/pageb',component:pageB},
+        {path:'/createBigScreen',component:createBigScreen},
+        {path:'/myReport',component:myReport}
+    ],
 })
 
 export default router
