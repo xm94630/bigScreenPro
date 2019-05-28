d7b984e318ffe4d8ba302a8be3c043cf806f80f4
xm94630 <xm94630@126.com>
2019-05-28 18:20:13 +0800

    代码优化

--- vue.config.js
+++ vue.config.js
@@ -10,22 +10,16 @@ module.exports={
     configureWebpack: {
         devServer: {
             proxy: {
-                '/koa': {
-                    target: 'http://localhost:3000',       //前端代理目标地址（通常是 3000端口 的koa服务，方便前端自己调试）
-                    //target: 'http://172.16.28.46:9090',   //后端接口地址   （解决和后端联调的跨域问题，暂时用不到，因为后端直接设置了允许跨域的请求头）
-                    
-                    //pathRewrite: {'^/api' : ''},      
-                    //changeOrigin: true,                   // target是域名的话，需要这个参数
-                    secure: false,                          // 设置支持https协议的代理
-                },
                 '/epimetheus': {
-                    //target: 'http://localhost:3000',
+                    target: 'http://localhost:3000',
+                    
                     //target: 'http://172.16.28.85:80',     // 德柱
                     
-                    target: 'http://172.18.1.118:8083',     // 苏州 志超
-                    pathRewrite: {'^/epimetheus' : ''}, 
+                    //target: 'http://172.18.1.118:8083',     // 苏州 志超
+                    //pathRewrite: {'^/epimetheus' : ''}, 
                     
                     secure: false, 
+                    //changeOrigin: true,                   // target是域名的话，需要这个参数
                 },
             }
         },
