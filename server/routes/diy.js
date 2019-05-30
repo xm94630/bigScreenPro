aebd16d8b9d1745a48a5a5bf812cb0f1b6ba13b1
xm94630 <xm94630@126.com>
2019-05-30 16:36:42 +0800

    香奈儿 也用上前端定义的条件查询

--- server/routes/diy.js
+++ server/routes/diy.js
@@ -907,7 +907,79 @@ router.get('/view/info', async (ctx, next) => {
             "dataUrl": "/epimetheus/api/diy/report/selectData",
             "initUrl": "/epimetheus/api_v1/diy/column/initForView",
             "diyCoreCode":"chanel-OutboundEfficiency"
-          }]
+          }],
+          "initForView":{
+            "conditionColumnList":[{
+              "label": "普通输入框 (type:10)",
+              "keyName": "name1",
+              "type": "10",
+              "typeScribe": "beeInput",
+              "placeholder": "请输入",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 0
+            },{
+              "label": "范围输入框 (type:11)",
+              "keyName": "name2",
+              "type": "11",
+              "typeScribe": "beeInputRange",
+              "placeholder": "请输入",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 1
+            },{
+              "label": "普通下拉 (type:20)",
+              "keyName": "love",
+              "type": "20",
+              "typeScribe": "beeSelect",
+              "placeholder": "请选择",
+              "defaultValue": "2",
+              "rule": {},
+              "referenceUrl":"/epimetheus/api_v1/diy/xxx/xxx",
+              "referenceColumn": "playerNameValue",
+              "referenceDisplayColumn": "playerName",
+              "queryIndex": 2
+            }, {
+              "label": "日期 (type:30)",
+              "keyName": "date1",
+              "type": "30",
+              "typeScribe": "beeDatePicker",
+              "placeholder": "请选择",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 3
+            }, {
+              "label": "日期（范围） (type:31)",
+              "keyName": "date2",
+              "type": "31",
+              "typeScribe": "beeDatePickerRange2",
+              "placeholder": ['开始','结束'],
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 4
+            }, {
+              "label": "日期时间 (type:40)",
+              "keyName": "date3",
+              "type": "40",
+              "typeScribe": "DateTimePicker",
+              "placeholder": "请选择",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 6
+            }, {
+              "label": "日期时间（范围） (type:41)",
+              "keyName": "date4",
+              "type": "41",
+              "typeScribe": "DateTimePickerRange",
+              "placeholder": ['开始','结束'],
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 5
+            }],
+            "resultColumnList":[{
+              bbb:222
+            }]
+          }
         }]
       }
 
@@ -954,7 +1026,79 @@ router.get('/view/info', async (ctx, next) => {
             "dataUrl": "/epimetheus/api/diy/report/selectData",
             "initUrl": "/epimetheus/api_v1/diy/column/initForView",
             "diyCoreCode":"chanel-InboundEfficiency"
-          }]
+          }],
+          "initForView":{
+            "conditionColumnList":[{
+              "label": "普通输入框 (type:10)",
+              "keyName": "name1",
+              "type": "10",
+              "typeScribe": "beeInput",
+              "placeholder": "请输入",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 0
+            },{
+              "label": "范围输入框 (type:11)",
+              "keyName": "name2",
+              "type": "11",
+              "typeScribe": "beeInputRange",
+              "placeholder": "请输入",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 1
+            },{
+              "label": "普通下拉 (type:20)",
+              "keyName": "love",
+              "type": "20",
+              "typeScribe": "beeSelect",
+              "placeholder": "请选择",
+              "defaultValue": "2",
+              "rule": {},
+              "referenceUrl":"/epimetheus/api_v1/diy/xxx/xxx",
+              "referenceColumn": "playerNameValue",
+              "referenceDisplayColumn": "playerName",
+              "queryIndex": 2
+            }, {
+              "label": "日期 (type:30)",
+              "keyName": "date1",
+              "type": "30",
+              "typeScribe": "beeDatePicker",
+              "placeholder": "请选择",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 3
+            }, {
+              "label": "日期（范围） (type:31)",
+              "keyName": "date2",
+              "type": "31",
+              "typeScribe": "beeDatePickerRange2",
+              "placeholder": ['开始','结束'],
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 4
+            }, {
+              "label": "日期时间 (type:40)",
+              "keyName": "date3",
+              "type": "40",
+              "typeScribe": "DateTimePicker",
+              "placeholder": "请选择",
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 6
+            }, {
+              "label": "日期时间（范围） (type:41)",
+              "keyName": "date4",
+              "type": "41",
+              "typeScribe": "DateTimePickerRange",
+              "placeholder": ['开始','结束'],
+              "defaultValue": "",
+              "rule": {},
+              "queryIndex": 5
+            }],
+            "resultColumnList":[{
+              bbb:222
+            }]
+          }
         }]
       }
 
