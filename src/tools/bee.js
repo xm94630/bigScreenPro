b78fb9c82a067421a637ada575f9228248111920
xm94630 <xm94630@126.com>
2019-06-07 09:57:38 +0800

    完成ObjToCSS 函数，验证完毕

--- src/tools/bee.js
+++ src/tools/bee.js
@@ -1,6 +1,6 @@
 // 通用函数库
-let bee = {}
-bee = (function(){
+var bee = (function(){
+
     //删除对象中值为空的key
     bee.effectiveKeys = function (obj){
         for(let key in obj){
@@ -10,6 +10,25 @@ bee = (function(){
         }
         return obj;
     }
+
+    //将对象格式的样式配置转换成css字符串形式。
+    bee.ObjToCSS = function(obj,otherCssStr){
+        let str = "";
+        let arr = ["width","height","top","bottom","left","right","font-size"];
+        for (let [key, value] of Object.entries(obj)) {
+            if(arr.indexOf(key)>-1 && ('number'===typeof(value) || value.indexOf('px')===-1)){
+                str += (`${key}:${value}px;`);
+            }else{
+                str += (`${key}:${value};`);
+            }
+        }
+        str += otherCssStr || '';
+        return str;
+    };
+
+
+
+
 
 
 
