81fa09b129b7c2a0e14f6adb37b3ebfe544cfb71
xm94630 <xm94630@126.com>
2019-06-20 15:13:49 +0800

    小改动，条件查询form表单的label位置 默认在上方

--- src/store.js
+++ src/store.js
@@ -6,7 +6,7 @@ Vue.use(Vuex)
 const state = {
   store_currentPage: 0,     //当前页数
   store_globalContion: {},  //全局条件查询配置
-  store_labelPosition: 'left',  //条件查询form表单的label位置控制
+  store_labelPosition: 'top',  //条件查询form表单的label位置控制
   store_colSpan: 8,        //条件查询form表单的元素之前的距离控制（每行放几个，一个24个栅格，设置8表示可以放3个元素）
   selectedWidgetId: '',        //选择中的组件id
 }
