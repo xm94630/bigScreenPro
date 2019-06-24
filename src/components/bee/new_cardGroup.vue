e7b7ddbe0626225d6b59c977c08773a04ac7c797
xm94630 <xm94630@126.com>
2019-06-24 09:56:25 +0800

    新增卡片组 组件

---
+++ src/components/bee/new_cardGroup.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="cardBox" :style="myCss" :name="myConfig.id">
+    卡片组合
+  </div>
+</template>
+
+<script>
+import bee from '@/src/tools/bee.js';
+import axios from "axios";
+import {baseUrl} from '@/bee.config';
+import store from '@/src/store';
+
+export default {
+  name: "new_cardGroup",
+  props: {
+    myConfig: null
+  },
+  data: function() {
+    return {
+    };
+  },
+  computed:{
+    myCss() {
+      let map = {"x":"left","y":"top"};
+      let cssObj = bee.replaceKey(this.myConfig.css,map);
+      let cssStr = bee.objToCSS(cssObj,"position:absolute;box-sizing:border-box;")
+      return cssStr;
+    }
+  },
+  methods:{
+    initWidget:function(myConfig){
+      this.diyCoreCode = myConfig.diyCoreCode;
+      let params = Object.assign({},{diyCoreCode:myConfig.diyCoreCode},store.state.store_globalContion);
+      //获取数据源
+      axios.post(baseUrl + myConfig.dataUrl,params).then(response => {
+        let apiData = response.data.data;
+        this.apiData = apiData;
+      });
+    },
+    updatedWidget:function(val){
+      let diyCoreCode = val.diyCoreCode;
+      //只有diyCoreCode发生改变的时候才调接口！
+      if(this.diyCoreCode!==diyCoreCode){
+        let dataUrl = val.dataUrl;
+        this.diyCoreCode=diyCoreCode;
+        let params = Object.assign({},{diyCoreCode:diyCoreCode},store.state.store_globalContion);
+        //获取数据源
+        axios.post(baseUrl + dataUrl,params).then(response => {
+          let apiData = response.data.data;
+          this.apiData = apiData;
+        });
+      }
+
+    },
+  },
+  watch:{
+    "myConfig":{
+      handler:function(newVal,oldVal){
+        this.updatedWidget(newVal,oldVal)
+      },
+      deep: true
+    },
+    
+  },
+  mounted: function() {
+    this.initWidget(this.myConfig);
+  },
+  updated(){
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
