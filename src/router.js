6855ee084dea39e31f78e07f9936afe71de9a3fe
xm94630 <xm94630@126.com>
2019-06-26 19:54:24 +0800

    mod

--- src/router.js
+++ src/router.js
@@ -1,7 +1,5 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
-//import pageA from './pages/a.vue';
-//import pageB from './pages/b.vue';
 import createBigScreen from './pages/createBigScreen.vue';
 import createBigScreen2 from './pages/createBigScreen2.vue';
 import pageIndex from './pages/indexPage.vue';
@@ -12,29 +10,26 @@ import bus from '@/src/tools/bus.js';
 
 Vue.use(VueRouter)
 
-
 const router = new VueRouter({
-    routes:[
-        {path:'/',redirect: '/index/bigscreen'},
-        {
-          path:'/index',
-          component:pageIndex,
-          children: [
-            {path: 'bigscreen',component: pageIndexBigScreen},
-            {path: 'dashboard',component: pageIndexDashboard},
-          ],
-          //前置守卫
-          beforeEnter:function (to, from, next){
-            bus.$off('widgetEvent');
-            next();
-          }
-        },
-        {path:'/createBigScreen',component:createBigScreen},
-        {path:'/createBigScreen2',component:createBigScreen2},
-        {path:'/myReport',component:myReport}
-    ],
+  routes:[
+    {path:'/',redirect: '/index/bigscreen'},
+    {
+      path:'/index',
+      component:pageIndex,
+      children: [
+        {path: 'bigscreen',component: pageIndexBigScreen},
+        {path: 'dashboard',component: pageIndexDashboard},
+      ],
+      //前置守卫
+      beforeEnter:function (to, from, next){
+        bus.$off('widgetEvent');
+        next();
+      }
+    },
+    {path:'/createBigScreen',component:createBigScreen},
+    {path:'/createBigScreen2',component:createBigScreen2},
+    {path:'/myReport',component:myReport}
+  ],
 })
 
-
 export default router
-
