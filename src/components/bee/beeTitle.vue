de0d42410244843247997fa57bd6577563e1ab3b
xuming <xuming@geekplus.com.cn>
2019-09-09 14:50:58 +0800

    标题组件也添加拖拽

--- src/components/bee/beeTitle.vue
+++ src/components/bee/beeTitle.vue
@@ -1,8 +1,16 @@
 <template>
-  <div class="beeTitle" :style="myCss" :name="myConfig.id" @click="clickFun(myConfig.id)">
+  <!-- 拖拽组件 -->
+  <vue-draggable-resizable 
+    :x="Number(myConfig.css.x)" :y="Number(myConfig.css.y)" :w="Number(myConfig.css.width)" :h="Number(myConfig.css.height)" 
+    :grid="grid" :parent="false"
+    v-on:dragging="onDrag" v-on:resizing="onResize" @activated="clickFun(myConfig.id)" 
+    class="widgetBox" :style="myCss" :name="myConfig.id" @click="clickFun(myConfig.id)"
+  >
+  <div class="widgetCon">
     {{myConfig.text}}
     <div :class="{selectBorder:myConfig.id===store.state.selectedWidgetId}"></div>
   </div>
+  </vue-draggable-resizable>
 </template>
 
 <script>
@@ -13,17 +21,39 @@ import store from '@/src/store';
 export default {
   name: "bee_title",//不能使用title，以为这个已经被默认占用了
   props: {
-    myConfig: null
+    myConfig: null,
+    canvasConfig: Object,
   },
   data() {
     return {
-      store
+      store,
+
+      //用于控制拖拽组件的初始定位
+      x: this.myConfig.css.x,
+      y: this.myConfig.css.y,
+      width: this.myConfig.css.width,
+      height: this.myConfig.css.height,
+      //初始的栅格
+      grid:(this.canvasConfig&&this.canvasConfig.grid)?[this.canvasConfig.grid,this.canvasConfig.grid]:[1,1]
     };
   },
   methods:{
+    onResize: function (x, y, width, height) {
+      this.myConfig.css.x = this.x = x
+      this.myConfig.css.y = this.y = y
+      this.myConfig.css.width =this.width = width
+      this.myConfig.css.height = this.height = height
+    },
+    onDrag: function (x, y) {
+      this.myConfig.css.x = this.x = x
+      this.myConfig.css.y = this.y = y   
+    },
     clickFun(widgetId){
-      store.dispatch("setSelectWidgetId",widgetId);
-      bus.$emit("widgetClick",widgetId);
+      //只有在编辑页面，这个点击才有效
+      if(this.$el.parentElement.id==="editCanvas"){
+        store.dispatch("setSelectWidgetId",widgetId);
+        bus.$emit("widgetClick",widgetId);
+      }
     }
   },
   computed: {
@@ -38,7 +68,17 @@ export default {
 </script>
 
 <style lang="scss">
-.beeTitle {
-  color: #000;
+.widgetBox {
+  position: absolute;
+  box-sizing: border-box;
+  .widgetCon {
+    width: 100%;
+    height: 100%;
+  }
+  .infoBox{
+    color:#4f8ff9;
+    font-size:12px;
+    margin-top: 5px;
+  }
 }
 </style>
