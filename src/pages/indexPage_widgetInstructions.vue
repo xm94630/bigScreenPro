f5b46231a6c4787ded59a7885fd587886298382c
xuming <xuming@geekplus.com.cn>
2019-10-14 16:36:57 +0800

    mod

--- src/pages/indexPage_widgetInstructions.vue
+++ src/pages/indexPage_widgetInstructions.vue
@@ -1,18 +1,43 @@
 <template>
-  <div>组件使用指南</div>
+  <div>
+    组件使用指南
+  <template>
+    <el-table
+      :data="tableData"
+      border
+      style="width: 100%">
+      <el-table-column
+        prop="xxx"
+        label="配置字段"
+        width="180"> 
+      </el-table-column>
+      <el-table-column
+        prop="yyy"
+        label="说明">
+      </el-table-column>
+    </el-table>
+  </template>
+  123
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'dashbord',
-  props: {
-    msg: String
-  },
-  data(){
-    return{
+    data() {
+      return {
+        tableData: [{
+          xxx: '2016-05-02',
+          yyy: '王小虎',
+        },{
+          xxx: '2016-05-02',
+          yyy: '王小虎',
+        },{
+          xxx: '2016-05-02',
+          yyy: '王小虎',
+        }]
+      }
     }
-  },
-}
+  }
 </script>
 
 <style scoped>
